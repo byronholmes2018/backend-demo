633f5884ae513788343307d02996de67ae8def07
ByronHolmes-at-129020800614 <lshopeful2018@gmail.com>
2021-09-07 20:53:44 -0500

    some more changes

--- routes/demo.js
+++ routes/demo.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+  res.status(200).send({data:'Good job, you connected'});
 });
 
 module.exports = router;
